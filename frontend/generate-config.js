330f5ec00593198b56e9dbfe47add1f254a26df9
RizwanSolace <rizwansolace31@gmail.com>
2026-09-23 17:21:22 +0530

    Changes

---
+++ frontend/generate-config.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+
+const apiUrl = process.env.API_URL;
+
+if (!apiUrl) {
+    throw new Error("API_URL environment variable is not set");
+}
+
+const template = fs.readFileSync("./config.template.js", "utf8");
+
+const config = template.replace("__API_URL__", apiUrl);
+
+fs.writeFileSync("./config.js", config);
+
+console.log("config.js generated successfully");
