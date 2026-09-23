a61c628420e1ab6a5a78d0e8565dad32ae231792
RizwanSolace <rizwansolace31@gmail.com>
2026-09-23 18:08:33 +0530

    Done

--- frontend/generate-config.js
+++ frontend/generate-config.js
@@ -6,7 +6,7 @@ if (!apiUrl) {
     throw new Error("API_URL environment variable is not configured");
 }
 
-const template = fs.readFileSync("./config.template.js", "utf8");
+const template = fs.readFileSync("./config-template.js", "utf8");
 
 const config = template.replace("__API_URL__", apiUrl);
 
